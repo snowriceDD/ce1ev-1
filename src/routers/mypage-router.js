8aec99ebfcf5a39cac6a9393bba868b9be03d4f8
donggram2 <113027954+donggram2@users.noreply.github.com>
2022-11-06 14:09:20 +0900

    Merge All

---
+++ src/routers/mypage-router.js
@@ -0,0 +1,8 @@
+import { Router } from "express";
+// import { productModel } from "../models";
+// import { userModel } from "../models";
+// import { mypageService } from "../services/mypage-service";
+
+const mypageRouter = Router();
+
+export { mypageRouter };
--- src/routers/product-router.js
+++ src/routers/product-router.js
@@ -1,15 +1,14 @@
 import { Router } from "express";
-import {ProductModel} from "../models"
+import { ProductModel } from "../models";
 import { productService } from "../services/product-service";
-
 
 const productRouter = Router();
 
-productRouter.get('/products', async (req, res) => { 
-    const product = await productService.findProudct(); //[{..}, {..}, ..]
+productRouter.get("/products", async (req, res) => {
+  const product = await productService.findProudct(); //[{..}, {..}, ..]
 
-    // res.render('template/postProduct', {product})
-    res.json(product)
+  // res.render('template/postProduct', {product})
+  res.json(product);
 });
 
 // productRouter.get('/:id', async (req, res)=> {
@@ -18,51 +17,61 @@ productRouter.get('/products', async (req, res) => {
 //     res.json(product);
 // })
 
-productRouter.get('/:category', async (req, res)=> {
-    const category = req.params.category;
-    const data = await productService.findCategory(category) // [{ brand: 5252 바이 오아이오아이, name: SIGNAUTRE HOODIE, price: 79,000}, {...}, ...]
+productRouter.get("/products/:category", async (req, res) => {
+  const category = req.params.category;
+  const data = await productService.findCategory(category); // [{ brand: 5252 바이 오아이오아이, name: SIGNAUTRE HOODIE, price: 79,000}, {...}, ...]
 
-    res.json(data)
+  res.json(data);
 });
 
-productRouter.get('/products/:brand', async (req, res)=> {
-    const brand = req.params.brand;
-    const data = await productService.findBrand(brand) // [{ brand: 5252 바이 오아이오아이, name: SIGNAUTRE HOODIE, price: 79,000}, {...}, ...]
-
-    res.json(data)
-});
-
-productRouter.post('/products', async(req, res)=> {
-
-    const {brand, name, price, size, color, category, description, img} = req.body;
-    const newProduct = await productService.addProduct({
-        brand, 
-        name, 
-        price, 
-        size, 
-        color, 
-        category, 
-        description, 
-        img
-    })
-    //console.log(newProduct);//num 안들어감
-    res.json(newProduct);
-});
-
-
-productRouter.delete('/:num', async(req, res)=> {
+productRouter.get("/productDetail/:num", async (req, res) => {
     const num = req.params.num;
-    const product = await productRouter.deleteProduct(num);
-    res.json(product);
-})
+    const data = await productService.findNum(num); // [{ brand: 5252 바이 오아이오아이, name: SIGNAUTRE HOODIE, price: 79,000}, {...}, ...]
+  
+    res.json(data);
+  });
 
+productRouter.get("/products/:productId", async (req, res) => {
+  const productId = req.params.productId;
+  const data = await productService.findId(productId);
+
+  res.json(data);
+});
+
+productRouter.get("/products/:brand", async (req, res) => {
+  const brand = req.params.brand;
+  const data = await productService.findBrand(brand); // [{ brand: 5252 바이 오아이오아이, name: SIGNAUTRE HOODIE, price: 79,000}, {...}, ...]
+
+  res.json(data);
+});
+
+productRouter.post("/products", async (req, res) => {
+  const { brand, name, price, size, color, category, description, img } =
+    req.body;
+  const newProduct = await productService.addProduct({
+    brand,
+    name,
+    price,
+    size,
+    color,
+    category,
+    description,
+    img,
+  });
+  //console.log(newProduct);//num 안들어감
+  res.json(newProduct);
+});
+
+productRouter.delete("/:num", async (req, res) => {
+  const num = req.params.num;
+  const product = await productRouter.deleteProduct(num);
+  res.json(product);
+});
 
 // productRouter.get('/?query=name', async (req, res)=> {
 //     const name = req.query.name;
 //     const data = await Product.findByName({name});
 //     res.json(data);
 // });
-
-
 
 export { productRouter };
