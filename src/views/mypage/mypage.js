3cdea9f04ffdf5a5302e9d2217cbbb326d87e1ab
noorong <100012522+noorong@users.noreply.github.com>
2022-11-07 17:41:33 +0900

    재훈

---
+++ src/views/mypage/mypage.js
@@ -0,0 +1,28 @@
+const mypageAccount = document.querySelector("#mypageAccount_button");
+const mypageOrderList = document.querySelector("#mypageOrderList_button");
+const mypageCart = document.querySelector("#mypageCart_button");
+const mypageWithdrawal = document.querySelector("#mypageWithdrawal_button");
+
+function moveTomypageAccount() {
+  window.location.assign("/mypage/account");
+}
+
+mypageAccount.addEventListener("click", moveTomypageAccount);
+
+function moveTomypageOrderList() {
+  window.location.assign("/mypage/myPageOrderList");
+}
+
+mypageOrderList.addEventListener("click", moveTomypageOrderList);
+
+function moveTomypageCart() {
+  window.location.assign("/mypage/myPageOrderList");
+}
+
+mypageCart.addEventListener("click", moveTomypageCart);
+
+function moveTomypageWithdrawal() {
+  window.location.assign("/mypage/withdrawal");
+}
+
+mypageWithdrawal.addEventListener("click", moveTomypageWithdrawal);
