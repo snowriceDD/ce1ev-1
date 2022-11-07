3e0d62f488f030a32a1b9e9f7160b01c7e990cb2
Yunsu Kim <yunsukim@Yunsuui-MacBookAir.local>
2022-11-07 21:50:39 +0900

    주문관리 페이지-모달추가

--- src/views/admin_orders/admin_orders_script.js
+++ src/views/admin_orders/admin_orders_script.js
@@ -1,53 +1,70 @@
 //import 설정 필요
-import { xxx } from "";
-import * as Api from "";
+// import { xxx } from "";
+// import * as Api from "";
+// import { doc } from "prettier";
 
-const ordersCount = document.querySelector("#ordersCount");
-const prepareCount = document.querySelector("#prepareCount");
-const deliveryCount = document.querySelector("#deliveryCount");
-const completeCount = document.querySelector("#completeCount");
-const listing = document.querySelector(".listing");
+// const ordersCount = document.querySelector("#ordersCount");
+// const prepareCount = document.querySelector("#prepareCount");
+// const deliveryCount = document.querySelector("#deliveryCount");
+// const completeCount = document.querySelector("#completeCount");
+// const listing = document.querySelector(".listing");
 
 const modal = document.querySelector("#modal");
-const modalBackground = document.querySelector("#modalBackground");
+// const modalBackground = document.querySelector("#modalBackground");
 const modalCloseButton = document.querySelector("#modalCloseButton");
-const deleteCompleteButton = document.querySelector('#deleteCompleteButton');
-const deleteCancelButton = document.querySelector("#deleteCancelButton");
+// const deleteCompleteButton = document.querySelector('#deleteCompleteButton');
+// const deleteCancelButton = document.querySelector("#deleteCancelButton");
 
 //admimn checking 함수 필요
 
 //addEventListner 묶음 
-function addAllEvent() {
-    modalBackground.addEventListener("click", closeModal);
-    modalCloseButton.addEventListener("click", closeModal);
-    document.addEventListener("keydown", keyDownCloseModal);
-    deleteCompleteButton.addEventListener("click", deleteOrderDate);
-    deleteCancelButton.addEventListener("click", cancelDelete);
+// function addAllEvent() {
+//     modalBackground.addEventListener("click", closeModal);
+//     modalCloseButton.addEventListener("click", closeModal);
+    // document.addEventListener("keydown", keyDownCloseModal);
+//     deleteCompleteButton.addEventListener("click", deleteOrderDate);
+//     deleteCancelButton.addEventListener("click", cancelDelete);
+// }
+
+
+//modal용 script
+const order_cancel = document.getElementById("order_cancel_btn");
+function closeModal() {
+    modal.style.display = "none"
+}
+function openModal() {
+    modal.style.display = "flex"
 }
 
+/* 주문취소 누르면 모달 팝업 */
+order_cancel.addEventListener("click", e => {openModal()});
+
+/*esc close module*/
+document.addEventListener("keydown", keyDownCloseModal);
 function keyDownCloseModal(e) {
     if(e.keyCode === 27) {
         closeModal();
     }
 }
 
+/* X button close */
+modalCloseButton.addEventListener("click", e => {closeModal()});
 
-const order_cancel = document.querySelector(".order_cancel");
+/* 모달 외 영역으로 끄기 */
+modal.addEventListener("click", e => {
+    const evTarget = e.target
+    if(evTarget.classList.contains("modal-overlay")) {
+    closeModal()};
+})
 
-order_cancel.addEventListener('click', () => {
-    openModal();
-}); 
+// order_cancel.addEventListener('click', () => {
+//     openModal();
+// }); 
 
-function openModal() {
-    modal.classList.add("is-active");
-}
+// function openModal() {
+//     modal.classList.add("is-active");
+// }
 
-function closeModal() {
-    modal.classList.remove("is-active");
-}
-
-function keyDownCloseModal(e) {
-    if (e.keyCode === 27) {
-        closeModal();
-    }
-}
+// function closeModal() {
+//     modal.classList.remove("is-active");
+// }
