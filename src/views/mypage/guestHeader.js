5dd0c9c1069296e2163894e29f7063464fd808a6
miny <miny4971@gmail.com>
2022-11-09 23:03:33 +0900

    헤더 사용자 별로 변경

--- src/views/mypage/guestHeader.js
+++ src/views/mypage/guestHeader.js
@@ -1,6 +1,7 @@
 const body = document.querySelector(".body");
 
-window.onload = () => {
+window.addEventListener("load", () =>{
+  console.log(123);
   body.insertAdjacentHTML(
     "afterBegin",
     `
@@ -18,7 +19,7 @@ window.onload = () => {
           </button>
         </li>
       </ul>
-      <ul class="header_right" id="loginBox">
+      <ul class="header_right" id="guest">
         <li id="header_login">
             <a href="/login">
                 LOGIN
@@ -35,52 +36,44 @@ window.onload = () => {
             </a>
         </li>
       </ul>
+      <ul class="header_right hidden" id="user">
+      <li id="header_cart">
+            <a href="/mypage/myPageCart">
+                CART
+            </a>
+        </li>
+        <li id="header_myPage">
+            <a href="/mypage">
+                MY PAGE
+            </a>
+        </li>
+        <li id="header_logout">
+            <a href="/logout">
+              LOG OUT
+            </a>
+        </li>
+      </ul>
+      <ul class="header_right hidden" id="admin">
+        <li id="header_addProduct">
+        <a href="/products">
+          ADD PRODUCT
+        </a>  
+      </li>
+    <li id="header_mypage">
+      <a href="/mypage">
+        ADMIN PAGE
+      </a>  
+    </li>
+    <li id="header_logout">
+      <p id="logout">LOGOUT</p>
+    </li>
+      </ul>
     </header>
     `
   );
 
-  body.insertAdjacentHTML(
-    "beforeEnd",
-    `
-  <footer class="footer">
-  <address class="address_left">
-    <ul class="addressLink">
-      <li>
-        <a href="/" id="footer_logo">
-          Ce1ev.
-        </a>
-      </li>
-      <li>
-        <a href="#">개인정보 취급방침</a>
-      </li>
-      <li>
-        <a href="#">이용약관</a>
-      </li>
-    </ul>
+  
+}) 
+  
 
-    <div class="addressText">
-      <p>셀레브 주식회사 · 대표 김원송 사업자등록번호 : 000-00-00000</p>
-      <p>
-        사업장소재지 : 경기도 성남시 분당구 분당내곡로 131 판교테크원 타워1, 8층
-      </p>
-      <p>ⓒ CELEV. Corp.</p>
-    </div>
-  </address>
-  <address class="address_right">
-    <p>
-      <strong style="font-size: 20px; font-weight: 600">
-        고객센터 1588-0000
-      </strong>
-    </p>
-    <p>
-      운영시간 평일 11:00 - 18:00 (토∙일, 공휴일 휴무)
-      <br />
-      점심시간 평일 13:00 - 14:00
-      <br />
-      1:1 문의하기는 앱에서만 가능합니다.
-    </p>
-  </address>
-</footer>
-  `
-  );
-};
+
