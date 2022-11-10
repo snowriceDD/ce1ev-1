72e800f01af6969b8ac9f769ae60524a152e8822
noorong <100012522+noorong@users.noreply.github.com>
2022-11-11 03:42:55 +0900

    동하코드받음. 장바구완료, 결제진행중

--- src/models/schemas/order.js
+++ src/models/schemas/order.js
@@ -1,36 +1,42 @@
-const {Schema} = require('mongoose');
-import {SelectedProductSchema} from './SelectedProductSchema';
+const { Schema } = require("mongoose");
+import { SelectedProductSchema } from "./SelectedProductSchema";
 
-const OrderSchema = new Schema({
+const OrderSchema = new Schema(
+  {
     userId: {
-        type: String,
-        required: true,
+      type: String,
+      required: false,
     },
-    orderNumber: { //Date.now() + Math.random() ~~
-        type: Number, 
-        required: true,
+    orderNumber: {
+      //Date.now() + Math.random() ~~
+      type: Number,
+      required: true,
     },
-    products: [SelectedProductSchema],
+    products: {
+      type: Array,
+      required: true,
+    },
     cost: {
-        type: Number,
-        required: true,
+      type: Number,
+      required: true,
     },
     count: {
-        type: Number,
-        required: true,
+      type: Number,
+      required: true,
     },
     payMethod: {
-        type: String,
-        required: true,
+      type: String,
+      required: true,
     },
     status: {
-        type: String,
-        default: "주문완료"
-    }
-}, {
+      type: String,
+      default: "주문완료",
+    },
+  },
+  {
     collection: "order",
     timestamps: true,
-});
+  }
+);
 
 export { OrderSchema };
-
