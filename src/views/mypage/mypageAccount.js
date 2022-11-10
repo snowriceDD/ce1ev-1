67168f97e1e76e2721bc9d9a7b4ca8e0bbfa9e87
noorong <100012522+noorong@users.noreply.github.com>
2022-11-10 18:54:30 +0900

    카테고리관리 삭제, 카트 css 수정

--- src/views/mypage/mypageAccount.js
+++ src/views/mypage/mypageAccount.js
@@ -1,38 +1,28 @@
-import * as Api from "/api.js"
-import {checkLogin} from "../useful-functions.js"
+import * as Api from "/api.js";
+import { checkLogin } from "../useful-functions.js";
 
 const nameTag = document.querySelector(".name");
 const phoneNumTag = document.querySelector(".phoneNum");
-const emailTag = document.querySelector('.email');
+const emailTag = document.querySelector(".email");
 const addressTag = document.querySelector(".address");
 const updateBtn = document.querySelector(".update");
-const withdrawBtn = document.querySelector(".withdraw");
-
 
 checkLogin();
 addAllEvents();
 
 let userData;
 async function addAllEvents() {
-    userData = await Api.get("/api/user");
+  userData = await Api.get("/api/user");
 
-    const {email, name, phoneNum, address } = userData;
+  const { email, name, phoneNum, address } = userData;
 
-    nameTag.innerText = `${name}`;
-    phoneNumTag.innerText = `${phoneNum}`;
-    emailTag.innerText=`${email}`;
-    addressTag.innerText = `${address}`; 
+  nameTag.innerText = `${name}`;
+  phoneNumTag.innerText = `${phoneNum}`;
+  emailTag.innerText = `${email}`;
+  addressTag.innerText = `${address}`;
 }
-
-
-withdrawBtn.addEventListener("click", withDraw);
-
-function withDraw() {
-    window.location.assign("/mypage/withdrawal");
+function update() {
+  window.location.assign("/mypage/accountUpdate");
 }
 
 updateBtn.addEventListener("click", update);
-
-function update() {
-    window.location.assign("/mypage/myAccountUpdate");
-}
