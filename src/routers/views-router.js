3ca8bd59494fe71d1b0e5d6f268091121aaed8ae
noorong <100012522+noorong@users.noreply.github.com>
2022-11-28 20:07:21 +0900

    hyun: 관리자 상품관리 페이지 기능 구현
    - 상품 "이름" 클릭 시 수정페이지로 이동
    - 선택된 상품 값 받아오기
        - 사이즈, 색상은 수정 필요함!
    - 상품 등록, 수정, 삭제 버튼 클릭 시 해당 페이지로 이동

--- src/routers/views-router.js
+++ src/routers/views-router.js
@@ -46,6 +46,15 @@ viewsRouter.get("/admin/adminProductList", (req, res) => {
     path.join(__dirname, "../views/adminProductList/adminProductList.html")
   );
 });
+
+viewsRouter.get("/admin/adminProductDetail/:productId", (req, res) => {
+  res.sendFile(
+    path.join(__dirname, "../views/admin_ProductDetail/admin_ProductDetail.html")
+  );
+});
+
+viewsRouter.use("/adminProductDetail/:productId/updateProduct", serveStatic("updateProduct"));
+
 viewsRouter.use("/admin/adminOrder", (req, res) => {
   res.sendFile(path.join(__dirname, "../views/admin_orders/admin_orders.html"));
 });
@@ -71,7 +80,6 @@ viewsRouter.use("/notice/:postNo", (req, res) => {
 });
 
 viewsRouter.use("/productDetail/:productId/updateProduct", serveStatic("updateProduct"));
-
 
 // views 폴더의 최상단 파일인 rabbit.png, api.js 등을 쓸 수 있게 함
 viewsRouter.use("/", serveStatic(""));
